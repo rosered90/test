6af02821cbb804a40c70be1f40f4fa5acae7ece2
rose_redff <1404248584@qq.com>
2018-11-14 11:41:54 +0800

    python自动化测试初步体验

---
+++ test_automate1/test_automate1.py
@@ -0,0 +1,10 @@
+# _*_ coding:utf-8 _*_
+def inc(x):
+    return x + 1
+
+
+def test_answer():
+    assert inc(3) == 5
+
+# a = inc(3)
+# print a
---
+++ test_automate1/test_class.py
@@ -0,0 +1,10 @@
+# _*_ coding:utf-8 _*_
+
+class TestClass:
+	def test_one(self):
+		x = 'this'
+		assert 'h' in x
+
+	def test_two(self):
+		x = 'hello'
+		assert hasattr(x, 'check')
