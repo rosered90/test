5f4a09d3a78ae7c21c9bf52e367affa2339f9383
rose_redff <1404248584@qq.com>
2018-12-11 21:02:11 +0800

    pd

---
+++ test_show.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+
+def get_date_list(begin_date, end_date):
+
+	date_list = [x.strftime('%Y-%m-%d') for x in list(pd.date_range(start=begin_date, end=end_date))]
+
+	return date_list
+
+
